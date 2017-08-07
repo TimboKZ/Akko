3312b414888f5d89599fd5a676c7e76012c61c6c
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2017-08-07 01:54:19 +0100

    Setup Gulp project

---
+++ dist/akko.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2017
+ * @license GPL-3.0
+ */
+
+var Akko = function Akko() {
+    _classCallCheck(this, Akko);
+
+};
+//# sourceMappingURL=akko.js.map
---
+++ examples/README.md
@@ -0,0 +1,7 @@
+# Akko usage examples
+
+This folder contains example visualisers written on Akko. To view the examples, you will either have to setup a local server or follow the CodePen links listed below (i.e. simply double-clicking on HTML files won't work).
+
+# List of examples
+
+> Coming soon. 
